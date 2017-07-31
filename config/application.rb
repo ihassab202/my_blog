38bb2613b27fa5366c0b74ad54d735d80f7cbdcd
ihassab202 <i.hasab@robone.net>
2017-07-31 19:03:15 +0200

    solving precompiling error

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module MyBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.assets.initialize_on_precompile = false
-    config.load_defaults 5.1
+    #config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
