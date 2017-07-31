6555df374524f520dbb9b4fbe62a8e7d9867b3da
ihassab202 <i.hasab@robone.net>
2017-07-31 19:01:19 +0200

    solving precompiling error

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-config.assets.initialize_on_precompile = false
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module MyBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
